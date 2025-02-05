f87af0952d3c20e188faa0ba1afa2952db166280
jaeyoonjung(rexyoon) <137620925+rexyoon@users.noreply.github.com>
2025-02-05 22:12:21 +0900

    UserEntity Added

---
+++ src/main/java/com/store/entity/UserEntity.java
@@ -0,0 +1,37 @@
+package com.store.entity;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
